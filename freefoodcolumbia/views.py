1c72be388cb827b2899ad46bb3e438e7b3460a8a
Ephraim Park <ephiepark@hotmail.com>
2011-04-10 04:32:26 -0400

    forms added and whatever I have so far

--- freefoodcolumbia/views.py
+++ freefoodcolumbia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from freefoodcolumbia.models import Event
+import time
+from datetime import datetime
 
 def index(request):
 #  name = 'freefoodColumbia App' # whats this for?
@@ -17,7 +19,7 @@ def index(request):
     event_list2.append(event_list[e[1]])
   return render_to_response('trash.tmpl', {'event_list':event_list2}, context_instance=RequestContext(request))
 
-def parseTime(strTime):
+def parseTime(year, month, dateD, strTime):
   indexColon = strTime.rfind(":")
   if indexColon != -1:
     if strTime[indexColon-2:indexColon-1].isdigit():
@@ -38,8 +40,7 @@ def parseTime(strTime):
     if hour < 9:
       hour += 12
   
-  time = hour * 60 + minute
-  return time
+  return datetime(year, month, dateD, hour, minute)
 
 def form(request):
   if 'datetime' in request.GET and request.GET['datetime']:
@@ -48,18 +49,13 @@ def form(request):
   return render_to_response('form.tmpl')
   
 def parseDate(date_list):
-  monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct","Nov","Dec"]
-  dateD = int(date_list.date[5:7])
-  numberMonth = 1
-  month = 1
-#  raise Exception(date_list.date[8:11])
-  for nameMonth in monthName:
-    if nameMonth == date_list.date[8:11]:
-      month = numberMonth
-    numberMonth = numberMonth + 1
-  year = int(date_list.date[12:16])
-  time = parseTime(date_list.date[20:])
-  return dateD*24*60 + month*24*60*30 + year*24*60*365 + time
+  temp = str(date_list.date[4:16]).lstrip()
+  temp = temp.rstrip()
+  year = time.strptime(temp,"%d %b %Y").tm_year
+  month = time.strptime(temp,"%d %b %Y").tm_mon
+  dateD = time.strptime(temp,"%d %b %Y").tm_mday
+  time1 = parseTime(year,month,dateD,date_list.date[19:])
+  return time1
   
   
 # Create your views here.
